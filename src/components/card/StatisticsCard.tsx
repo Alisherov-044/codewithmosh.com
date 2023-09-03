81ef0aa60082db47c9a1a2d7239f4e7963962a2a
fireship <fireship@wtf.fireship>
2023-09-04 01:05:27 +0500

    fixed: feature card color issue

--- src/components/card/StatisticsCard.tsx
+++ src/components/card/StatisticsCard.tsx
@@ -3,21 +3,20 @@ import { StatisticsCardInterface } from "@/interface";
 export function StatisticsCard({ statistic }: StatisticsCardInterface) {
   const { title, description, color } = statistic;
 
-  const viaColor = `via-[${color}]`;
-  const textColor = `text-[${color}]`;
-
   return (
     <div className="shadow-2xl dark:shadow-none border border-blue-950 bg-[#11152C] rounded-md relative">
       <div className="flex justify-center -translate-y-[1px]">
         <div className="w-3/4">
           <div
-            className={`h-[1px] bg-gradient-to-r from-transparent to-transparent w-full ${viaColor}`}
+            style={{ color }}
+            className={`h-[1px] bg-gradient-to-r from-transparent to-transparent w-full via-[${color}]`}
           ></div>
         </div>
       </div>
       <div className="flex flex-col justify-center items-center text-center p-8">
         <h2
-          className={`font-bold mb-2 text-2xl md:text-3xl lg:text-4xl ${textColor}`}
+          style={{ color }}
+          className={`font-bold mb-2 text-2xl md:text-3xl lg:text-4xl`}
         >
           {title}
         </h2>
