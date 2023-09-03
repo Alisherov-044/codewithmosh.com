46577878bdf2a51a54fdd98f483c5e2c67c3de91
fireship <fireship@wtf.fireship>
2023-09-04 00:44:44 +0500

    cleaned project

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,15 +1,5 @@
-import { courses } from "./courses";
-import { features } from "./features";
 import { navbarLinks } from "./navLinks";
-import { statistics } from "./Statistics";
 import { socialLinks } from "./socialLinks";
 import { adminNavbarLinks } from "./adminNavLinks";
 
-export {
-  courses,
-  features,
-  statistics,
-  socialLinks,
-  navbarLinks,
-  adminNavbarLinks,
-};
+export { socialLinks, navbarLinks, adminNavbarLinks };
